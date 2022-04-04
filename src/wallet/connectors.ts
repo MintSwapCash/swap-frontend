7edeee97c15a5ff684e50df1cd0c467ca87c97a1
MintSwapCash <96653933+MintSwapCash@users.noreply.github.com>
2022-04-05 01:20:45 +0200

    Update connectors.ts

--- src/wallet/connectors.ts
+++ src/wallet/connectors.ts
@@ -1,3 +1,3 @@
 import { InjectedConnector } from '@web3-react/injected-connector'
 
-export const injected = new InjectedConnector({ supportedChainIds: [1,56,96,97,1337,24734] })
+export const injected = new InjectedConnector({ supportedChainIds: [24734] })
