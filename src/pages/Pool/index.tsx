e080d8c358356541bc6cdfa83b2a6b4350201b6f
MintSwapCash <96653933+MintSwapCash@users.noreply.github.com>
2022-04-05 13:47:46 +0200

    Update index.tsx

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -12,10 +12,10 @@ export default function Pool() {
             <div className="mt-3 d-flex flex-wrap">
                 <PoolIcon 
                     token1="WMINT" 
-                    token2="LEMON"
+                    token2="XTR"
                     address="0x427c8f51a2598082a5B61213e293a8474C08150E"
                     abi={[]}
-                    onClick={() => history.push("/pool/WBNB-LEMON")}
+                    onClick={() => history.push("/pool/WMINT-XTR")}
                     
                     />
                 <PoolIcon 
@@ -28,18 +28,18 @@ export default function Pool() {
                     />
                 <PoolIcon 
                     token1="WMINT" 
-                    token2="TIG"
+                    token2="ANUK"
                     address="0x27835abf87e682d187e871871fA98764" // MOCK
                     abi={[]}
-                    onClick={() => history.push("/pool/WBNB-TIG")}
+                    onClick={() => history.push("/pool/WMINT-ANUK")}
                     
                     />
                 <PoolIcon 
                     token1="WMINT" 
-                    token2="GG"
+                    token2="CONT"
                     address="0x27835abf87e682d187e871871fA98764" // MOCK
                     abi={[]}
-                    onClick={() => history.push("/pool/WBNB-GG")}
+                    onClick={() => history.push("/pool/WMINT-CONT")}
                     
                     />
 
