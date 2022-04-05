8bf522b3974d782460c9db7514edc53666232ea3
MintSwapCash <96653933+MintSwapCash@users.noreply.github.com>
2022-04-05 13:19:55 +0200

    Update addresses.ts

--- src/utils/addresses.ts
+++ src/utils/addresses.ts
@@ -1,48 +1,37 @@
 export const factoryAddress = "0x2897b4089939f84f7df62095eafec6a95271f6f5";
-export const token1Address = "0xd50180Ca4E01e8a64A6acD7A601E7F807c0d9d85";
-export const token2Address = "0x07836EfbD63772E9dE784A550FE4de3441e430B4";
 export const prssAddress = "0xFe56deBf29Cab1a641543107e17c8d6225F9f626";
-export const lemonAddress = "0x0BF2739eE043E8d9e227929d3E81e4a6Da1961cb";
-export const goodgameAddress = "0xcFB940CA511799AF60d8cA3534A1467927080008";
-export const tigraAddress = "0xF39364835b9E7C11E834B6CFAFfA52ab168E3b28";
-
-export const wbnbAddress = "0x81da10dfc2c7f8185144df2bd5f950b02b561d46";
+export const continentalAddress = "0x8D71f14aF3c689f60d554E51e1bf3015281B0B29";
+export const xatterAddress = "0xB580f1dbA1c17882Fca8f6DDadA8428c9cB177fC";
+export const anukisAddress = "0xfcC19E279D0240cFdaBdEEB6885f6829FCCfa501";
+export const wmintAddress = "0x81da10dfc2c7f8185144df2bd5f950b02b561d46";
 
 export const routerAddress = "0x1b553148f6d80e67efdce3ab5390ec41c7f3b7dc";
 
 
 export const Tokens: { [key: string]: { name: string, address: string}} = {
-    "TK1": {
-        "name": "Token1",
-        "address": "0xd50180Ca4E01e8a64A6acD7A601E7F807c0d9d85"
-    },
-    "TK2": {
-        "name": "Token2",
-        "address": "0x07836EfbD63772E9dE784A550FE4de3441e430B4"
-    },
     "PRSS": {
         "name": "PRSS",
         "address": "0xFe56deBf29Cab1a641543107e17c8d6225F9f626"
     },
-    "LEMON": {
-        "name": "LemonCoin",
-        "address": "0x0BF2739eE043E8d9e227929d3E81e4a6Da1961cb"
+    "XTR": {
+        "name": "Xatter",
+        "address": "0xB580f1dbA1c17882Fca8f6DDadA8428c9cB177fC"
     },
-    "GG": {
-        "name": "GoodGame",
-        "address": "0xcFB940CA511799AF60d8cA3534A1467927080008"
+    "CONT": {
+        "name": "Continental",
+        "address": "0x8D71f14aF3c689f60d554E51e1bf3015281B0B29"
     },
-    "TIG": {
-        "name": "TIGRA",
-        "address": "0xF39364835b9E7C11E834B6CFAFfA52ab168E3b28"
+    "ANUK": {
+        "name": "Anukis",
+        "address": "0xfcC19E279D0240cFdaBdEEB6885f6829FCCfa501"
     },
     "WMINT": {
         "name": "Wrapped MintMe",
         "address": "0x81da10dfc2c7f8185144df2bd5f950b02b561d46"
     },
-    "BNB": {
-        "name": "BNB",
-        "address": "0x50a8911ef76835E4D9dE395398bF0E2D89bee26B"
+    "MINTME": {
+        "name": "MINTME",
+        "address": "0x81da10dfc2c7f8185144df2bd5f950b02b561d46"
     },
     
 }
@@ -53,34 +42,24 @@ export const TokenOfAddress: { [key: string]: { name: string, symbol: string, ic
         "symbol": "WMINT",
         "icon": "",
     },
-    "0xd50180Ca4E01e8a64A6acD7A601E7F807c0d9d85": {
-        "name": "Token1",
-        "symbol": "TK1",
-        "icon": "",
-    },
-    "0x07836EfbD63772E9dE784A550FE4de3441e430B4": {
-        "name": "Token2",
-        "symbol": "TK2",
-        "icon": "",
-    },
     "0xFe56deBf29Cab1a641543107e17c8d6225F9f626": {
         "name": "PRSS",
         "symbol": "PRSS",
         "icon": "https://static.coinpaprika.com/coin/prss-prss/logo.png",
     },
-    "0x0BF2739eE043E8d9e227929d3E81e4a6Da1961cb": {
-        "name": "LemonCoin",
-        "symbol": "LEMON",
-        "icon": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYsA3einMBqtVqApENbhx1vM4vl-dynyoypA&usqp=CAU",
+    "0xB580f1dbA1c17882Fca8f6DDadA8428c9cB177fC": {
+        "name": "Xatter",
+        "symbol": "XTR",
+        "icon": "https://www.mintme.com/media/cache/avatar_large/uploads/images/05dd4214-dc43-11eb-89a0-78e3b517e592.jpeg",
     },
-    "0xcFB940CA511799AF60d8cA3534A1467927080008": {
-        "name": "GoodGame",
-        "symbol": "GG",
-        "icon": "https://t3.ftcdn.net/jpg/03/24/46/72/360_F_324467283_97K5JaR0Ko8BYjuzS3UlpGgVTilVw0Iv.jpg",
+    "0x8D71f14aF3c689f60d554E51e1bf3015281B0B29": {
+        "name": "Continental",
+        "symbol": "CONT",
+        "icon": "https://earnerschannel.pl/img/Continental.png",
     },
-    "0xF39364835b9E7C11E834B6CFAFfA52ab168E3b28": {
-        "name": "TIGRA",
-        "symbol": "TIG",
-        "icon": "https://static.wikia.nocookie.net/symbolism/images/4/43/Orange.png/revision/latest/scale-to-width-down/400?cb=20140818120046",
+    "0xfcC19E279D0240cFdaBdEEB6885f6829FCCfa501": {
+        "name": "Anukis",
+        "symbol": "ANUK",
+        "icon": "https://www.mintme.com/media/cache/avatar_large/uploads/images/d79c0b3a-099b-11eb-ae0b-78e3b517e592.png",
     },
 }
