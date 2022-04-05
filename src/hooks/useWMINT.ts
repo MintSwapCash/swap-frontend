7bf4d8993f8dc4fcb2ebdb66f1f68eb9177e6bf5
MintSwapCash <96653933+MintSwapCash@users.noreply.github.com>
2022-04-05 13:37:21 +0200

    Update and rename useWBNB.ts to useWMINT.ts

--- src/hooks/useWMINT.ts
+++ src/hooks/useWMINT.ts
@@ -1,8 +1,8 @@
 import { web3 } from "../wallet/providers/web3";
 import { AbiItem } from 'web3-utils';
-import { abi } from "../utils/abi/WBNB.json";
+import { abi } from "../utils/abi/WMINT.json";
 
-export default function useWBNB(tokenAddress: string) {
+export default function useWMINT(tokenAddress: string) {
     const Token = new web3.eth.Contract(abi as AbiItem[], tokenAddress);
     return Token;
 }
